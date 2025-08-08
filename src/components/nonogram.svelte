8c6a3c58acca194cff1f64f07ad48585db4c8ad4
Adam Grzeszczuk <adameq02@outlook.com>
2025-08-08 12:39:21 +0200

    feat: random game generation

--- src/components/nonogram.svelte
+++ src/components/nonogram.svelte
@@ -1,23 +1,42 @@
 <script lang="ts">
-    let hints_x = [[1],[1,1],[1],[4]];
-    let hints_y = [[1,1],[1,1],[1],[3]]; 
-    let row = {
-        horizontal: hints_x.map((_, x) => x),
-        vertical: hints_y.map((_, y) => y)
-    }
-    let grid = hints_x.map(_ => hints_y.map(_ => 0));
+    let hints_x: number[][] = [];
+    let hints_y: number[][] = [];
+    let grid: number[][] = [];
+    let rows: {
+        horizontal: number[], 
+        vertical: number[]
+    } = {
+        horizontal: [], 
+        vertical: []
+    };
+    
     let initialHit: number;
     let isDragged = false;
-
-    const drag = (x: number, y: number) => {
+    
+    const drag = (event: MouseEvent, x: number, y: number) => {
         isDragged = true;
 
         switch(grid[x][y]) {
             case 0:
-                initialHit = 1;
+                if (event.button === 0) {
+                    initialHit = 1;
+                } else if (event.button === 2) {
+                    initialHit = -1;
+                }
                 break;
             case 1:
-                initialHit = 0;
+                if (event.button === 0) {
+                    initialHit = 0;
+                } else if (event.button === 2) {
+                    initialHit = -1;
+                }
+                break;
+            case -1:
+                if (event.button === 0) {
+                    initialHit = 1;
+                } else if (event.button === 2) {
+                    initialHit = 0;
+                }
                 break;
         }
 
@@ -29,110 +48,176 @@
             grid[x][y] = initialHit;
         }
 
-        const checkedX: number[] = [];
+        const tempX: number[] = [];
         let sumX = 0;
 
         for (let col = 0; col < grid[x].length; col += 1) {
             if (grid[x][col] === 1) {
                 sumX += 1;
-            } else if (grid[x][col] === 0 && sumX > 0) {
-                checkedX.push(sumX);
+            } else if (grid[x][col] <= 0 && sumX > 0) {
+                tempX.push(sumX);
                 sumX = 0;
             }
         }
         if (sumX > 0) {
-            checkedX.push(sumX);
+            tempX.push(sumX);
         }
 
-        const checkedY: number[] = [];
+        const tempY: number[] = [];
         let sumY = 0;
 
         for (let row = 0; row < grid.length; row += 1) {
             if (grid[row][y] === 1) {
                 sumY += 1;
-            } else if (grid[row][y] === 0 && sumY > 0) {
-                checkedY.push(sumY);
+            } else if (grid[row][y] <= 0 && sumY > 0) {
+                tempY.push(sumY);
                 sumY = 0;
             }
         }
         if (sumY > 0) {
-            checkedY.push(sumY);
+            tempY.push(sumY);
         }
 
-        if (checkedX.toString() === hints_x[x].toString()) {
-            row.horizontal = row.horizontal.filter(number => number !== x);
-        } else if (!row.horizontal.includes(x)) {
-            row.horizontal = [x, ...row.horizontal]
+        if (tempX.toString() === hints_x[x].toString()) {
+            rows.horizontal = rows.horizontal.filter(number => number !== x);
+        } else if (!rows.horizontal.includes(x)) {
+            rows.horizontal = [x, ...rows.horizontal]
         }
 
-        if (checkedY.toString() === hints_y[y].toString()) {
-            row.vertical = row.vertical.filter(number => number !== y);
-        } else if (!row.vertical.includes(y)) {
-            row.vertical = [y, ...row.vertical]
+        if (tempY.toString() === hints_y[y].toString()) {
+            rows.vertical = rows.vertical.filter(number => number !== y);
+        } else if (!rows.vertical.includes(y)) {
+            rows.vertical = [y, ...rows.vertical]
         }
+    }
+
+    export const random = (size: number) => {
+        hints_x = [];
+        hints_y = [];
+        grid = [];
+
+        const probability = 0.5;
+        const tempGrid = Array.from({length: size}).map(_ => Array.from({length: size}).map(_ => 0));
+
+        for (let row = 0; row < size; row++) {
+            for (let col = 0; col < size; col++) {
+               tempGrid[row][col] = Math.random() < probability ? 1 : 0;
+            }
+        }
+
+        let tempX: number[] = [];
+        for (let row = 0; row < tempGrid.length; row += 1) {
+            let sum = 0;
+
+            for (let col = 0; col < tempGrid[row].length; col += 1) {
+                if (tempGrid[row][col] === 1) {
+                    sum += 1;
+                } else if (tempGrid[row][col] <= 0 && sum > 0) {
+                    tempX.push(sum);
+                    sum = 0;
+                }
+            }
+            if (sum > 0) tempX.push(sum);
+
+            hints_x.push(tempX);
+            tempX = []; 
+        }
+
+        let tempY: number[] = [];
+        for (let col = 0; col < tempGrid.length; col += 1) {
+            let sum = 0;
+
+            for (let row = 0; row < tempGrid[col].length; row += 1) {
+                if (tempGrid[row][col] === 1) {
+                    sum += 1;
+                } else if (tempGrid[row][col] <= 0 && sum > 0) {
+                    tempY.push(sum);
+                    sum = 0;
+                }
+            }
+            if (sum > 0) tempY.push(sum);
+
+            hints_y.push(tempY);
+            tempY = []; 
+        }   
+        
+        if (hints_x.some(arr => arr.length === 0) || hints_y.some(arr => arr.length === 0)) {
+            random(size);
+        }
+
+        rows = {
+            horizontal: hints_x.map((_, x) => x),
+            vertical: hints_y.map((_, y) => y)
+        }
+
+        grid = Array.from({length: size}).map(_ => Array.from({length: size}).map(_ => 0));
+    }
+
+    $: if (rows.horizontal.length === 0 && rows.vertical.length === 0) {
+        
     }
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <!-- svelte-ignore a11y_mouse_events_have_key_events -->
-<div 
-    class="w-fit grid grid-rows-[auto_1fr] grid-cols-[auto_1fr]" 
-    on:mouseup={() => isDragged = false}
->
-    <div></div>
-    <div class="flex items-end">
-        {#each hints_y as hint_y, index}
-            <div class="flex flex-col">
-                {#each hint_y as hint}
-                    <div class="w-10 h-8 bg-black flex justify-center items-center">
-                        <span class="{row.vertical.includes(index) ? '' : 'opacity-20'}">
-                            {hint}
-                        </span>
-                    </div>
-                {/each}
-            </div>
-        {/each}
-    </div>
-    <div class="flex flex-col">
-        {#each hints_x as hint_x, index}
-            <div class="flex justify-end">
-                {#each hint_x as hint}
-                    <div class="w-8 h-10 bg-black flex justify-center items-center">
-                        <span class="{row.horizontal.includes(index) ? '' : 'opacity-20'}">
-                            {hint}
-                        </span>
-                    </div>
-                {/each}
-            </div>
-        {/each}
-    </div>
-    <div class="divide-y-2 divide-black grid border-2 border-black bg-white">
-        {#each {length: hints_x.length}, x}
-            <div class="flex divide-x-2 divide-black">
-                {#each {length: hints_y.length}, y}
-                    <button
-                        on:mousedown={() => drag(x, y)}
-                        on:mouseover={() => {if (isDragged) fill(x, y)}}
-                        aria-label="grid button"
-                        class="{grid[x][y] === 1 ? 'x' : ''} bg-white relative w-full h-full overflow-hidden"
-                    >
-                        <div class="{row.horizontal.includes(x) ? '' : 'line-x'} {row.vertical.includes(y) ? '' : 'line-y'} opacity-30"></div>
-                    </button>
-                {/each}
-            </div>
-        {/each}
-    </div>
-    <div class="fixed grid gap-4 left-4 bottom-4 bg-black p-2">
-        <h1 class="text-center p-2 bg-white/5 -m-2">
-            Incorrect rows and columns
-        </h1>
-        <div class="grid grid-cols-[auto_1fr] gap-x-4 p-2">
-            <div class="text-right">
+{#if grid.length > 0}
+    <div 
+        class="w-fit grid grid-rows-[auto_1fr] grid-cols-[auto_1fr]" 
+        on:mouseup={() => isDragged = false}
+        on:mouseleave={() => isDragged = false}
+        on:keypress={() =>  null}
+    >
+        <div></div>
+        <div class="flex items-end">
+            {#each hints_y as hint_y, index}
+                <div class="flex flex-col">
+                    {#each hint_y as hint}
+                        <div class="w-10 h-8 bg-black flex justify-center items-center">
+                            <span class="{rows.vertical.includes(index) ? '' : 'opacity-20'}">
+                                {hint}
+                            </span>
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+        <div class="flex flex-col">
+            {#each hints_x as hint_x, index}
+                <div class="flex justify-end">
+                    {#each hint_x as hint}
+                        <div class="w-8 h-10 bg-black flex justify-center items-center">
+                            <span class="{rows.horizontal.includes(index) ? '' : 'opacity-20'}">
+                                {hint}
+                            </span>
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+        <div class="divide-y-2 divide-black grid border-2 border-black bg-white relative">
+            {#each {length: hints_x.length}, x}
+                <div class="flex divide-x-2 divide-black">
+                    {#each {length: hints_y.length}, y}
+                        <button
+                            on:contextmenu|preventDefault
+                            on:mousedown|preventDefault={(event) => drag(event, x, y)}    
+                            on:mouseover={() => {if (isDragged) fill(x, y)}}
+                            aria-label="grid button"
+                            class="{grid[x][y] === 1 ? '!bg-black/75' : (grid[x][y] === -1 ? 'x' : 'bg-white')} hover:bg-black/10 relative w-full h-full overflow-hidden"
+                        >
+                            <div class="{rows.horizontal.includes(x) ? '' : 'line-x'} {rows.vertical.includes(y) ? '' : 'line-y'} opacity-30"></div>
+                        </button>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+        <div class="fixed gap-2 left-4 bottom-4 bg-black grid grid-cols-[auto_1fr] p-2">
+            <div class="text-right my-auto">
                 Rows
             </div> 
             <div class="divide-x-2 divide-white/10 flex bg-white/5 p-1">
-                {#if row.horizontal.length > 0}
-                    {#each row.horizontal.sort() as el}
+                {#if rows.horizontal.length > 0}
+                    {#each rows.horizontal.sort() as el}
                         <span class="px-2">
                             {el + 1}
                         </span>
@@ -143,12 +228,12 @@
                     </span>
                 {/if}
             </div>
-            <div class="text-right">
+            <div class="text-right my-auto">
                 Columns
             </div>
             <div class="divide-x-2 divide-white/10 flex bg-white/5 p-1">
-                {#if row.vertical.length > 0}
-                    {#each row.vertical.sort() as el}
+                {#if rows.vertical.length > 0}
+                    {#each rows.vertical.sort() as el}
                         <span class="px-2">
                             {el + 1}
                         </span>
@@ -161,20 +246,10 @@
             </div>
         </div>
     </div>
-</div>
+{/if}
 
 <style scoped>
-.x::before {
-  background-color: black;
-  content: "";
-  width: 2px;
-  height: 150%;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%) rotate(45deg);
-  position: absolute;
-}
-
+.x::before,
 .x::after {
   background-color: black;
   content: "";
@@ -182,29 +257,30 @@
   height: 150%;
   left: 50%;
   top: 50%;
+  position: absolute;
+}
+.x::before {
+  transform: translate(-50%, -50%) rotate(45deg);
+}
+.x::after {
   transform: translate(-50%, -50%) rotate(-45deg);
-  position: absolute;
 }
 
-.line-x::before {
-  background-color: red;
-  content: "";
-  width: 100%;
-  height: 2px;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  position: absolute;
-}
-
+.line-x::before,
 .line-y::after {
   background-color: red;
   content: "";
-  width: 2px;
-  height: 100%; 
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
   position: absolute;
+}
+.line-x::before {
+  width: 100%;
+  height: 2px;
+}
+.line-y::after {
+  width: 2px;
+  height: 100%;
 }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
 <script>
   import Nonogram from "../components/nonogram.svelte";
+  let nonogram = $state();
+  let size = $state();
+  let sizes = [
+    {name: '5x5', value: 5},
+    {name: '8x8', value: 8},
+    {name: '10x10', value: 10}
+  ]
 
+  function randomNonogram() {
+		nonogram.random(size)
+	}
 </script>
 
-<Nonogram></Nonogram>
+<div>
+  <div class="w-fit mx-auto mt-4">
+    <Nonogram bind:this={nonogram}></Nonogram>
+  </div>
+  <div class="fixed bottom-4 right-4">
+    <select class="bg-black px-4 py-2" bind:value={size}>
+      {#each sizes as size}
+        <option value={size.value}>{size.name}</option>
+      {/each}
+    </select>
+    <button class="bg-black px-4 py-2 cursor-pointer" onclick={randomNonogram}>New</button>
+  </div>
+</div>
